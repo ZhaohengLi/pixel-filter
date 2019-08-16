36b491d63232c32ee80726d7edd44317f7e91cf8
ZhaohengLi <zhaohengli.thu@gmail.com>
2019-08-16 12:54:48 +0800

    Add running data.

---
+++ cmake-build-debug/CMakeFiles/CVProjectV1_0.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/lizhaoheng/Dropbox/Work/CLionProjects/CVProjectV1.0/faceDetect.cpp" "/Users/lizhaoheng/Dropbox/Work/CLionProjects/CVProjectV1.0/cmake-build-debug/CMakeFiles/CVProjectV1_0.dir/faceDetect.cpp.o"
+  "/Users/lizhaoheng/Dropbox/Work/CLionProjects/CVProjectV1.0/facedetectcnn-floatdata.cpp" "/Users/lizhaoheng/Dropbox/Work/CLionProjects/CVProjectV1.0/cmake-build-debug/CMakeFiles/CVProjectV1_0.dir/facedetectcnn-floatdata.cpp.o"
+  "/Users/lizhaoheng/Dropbox/Work/CLionProjects/CVProjectV1.0/facedetectcnn-int8data.cpp" "/Users/lizhaoheng/Dropbox/Work/CLionProjects/CVProjectV1.0/cmake-build-debug/CMakeFiles/CVProjectV1_0.dir/facedetectcnn-int8data.cpp.o"
+  "/Users/lizhaoheng/Dropbox/Work/CLionProjects/CVProjectV1.0/facedetectcnn-model.cpp" "/Users/lizhaoheng/Dropbox/Work/CLionProjects/CVProjectV1.0/cmake-build-debug/CMakeFiles/CVProjectV1_0.dir/facedetectcnn-model.cpp.o"
+  "/Users/lizhaoheng/Dropbox/Work/CLionProjects/CVProjectV1.0/facedetectcnn.cpp" "/Users/lizhaoheng/Dropbox/Work/CLionProjects/CVProjectV1.0/cmake-build-debug/CMakeFiles/CVProjectV1_0.dir/facedetectcnn.cpp.o"
+  "/Users/lizhaoheng/Dropbox/Work/CLionProjects/CVProjectV1.0/main.cpp" "/Users/lizhaoheng/Dropbox/Work/CLionProjects/CVProjectV1.0/cmake-build-debug/CMakeFiles/CVProjectV1_0.dir/main.cpp.o"
+  "/Users/lizhaoheng/Dropbox/Work/CLionProjects/CVProjectV1.0/rectBlur.cpp" "/Users/lizhaoheng/Dropbox/Work/CLionProjects/CVProjectV1.0/cmake-build-debug/CMakeFiles/CVProjectV1_0.dir/rectBlur.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/CVProjectV1_0.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CVProjectV1_0.dir/main.cpp.o"
+  "CMakeFiles/CVProjectV1_0.dir/rectBlur.cpp.o"
+  "CMakeFiles/CVProjectV1_0.dir/faceDetect.cpp.o"
+  "CMakeFiles/CVProjectV1_0.dir/facedetectcnn-floatdata.cpp.o"
+  "CMakeFiles/CVProjectV1_0.dir/facedetectcnn-int8data.cpp.o"
+  "CMakeFiles/CVProjectV1_0.dir/facedetectcnn-model.cpp.o"
+  "CMakeFiles/CVProjectV1_0.dir/facedetectcnn.cpp.o"
+  "CVProjectV1_0.pdb"
+  "CVProjectV1_0"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CVProjectV1_0.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
